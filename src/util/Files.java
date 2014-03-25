14a23b10d1bb7eb194441507f42a03c583666011
Surbhi <surbhishah81@yahoo.in>
2014-03-25 11:49:15 +0530

    container classes completed

--- src/util/Files.java
+++ src/util/Files.java
@@ -3,7 +3,28 @@ package util;
 public class Files {
 	private String file;
 	private String parent;
+	private String name;
+	private String user_cloud_name;
+	private String user_cloudID;
 	
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public String getUser_cloud_name() {
+		return user_cloud_name;
+	}
+	public void setUser_cloud_name(String user_cloud_name) {
+		this.user_cloud_name = user_cloud_name;
+	}
+	public String getUser_cloudID() {
+		return user_cloudID;
+	}
+	public void setUser_cloudID(String user_cloudID) {
+		this.user_cloudID = user_cloudID;
+	}
 	public String getFile() {
 		return file;
 	}
--- src/util/Folders.java
+++ src/util/Folders.java
@@ -1,10 +1,36 @@
 package util;
 
-import java.util.Map;
 
 public class Folders {
 	
 	private String folder;
+	private String name;
+	private String user_cloud_name;
+	private String user_cloudID;
+	
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getUser_cloud_name() {
+		return user_cloud_name;
+	}
+
+	public void setUser_cloud_name(String user_cloud_name) {
+		this.user_cloud_name = user_cloud_name;
+	}
+
+	public String getUser_cloudID() {
+		return user_cloudID;
+	}
+
+	public void setUser_cloudID(String user_cloudID) {
+		this.user_cloudID = user_cloudID;
+	}
 
 	public String getFolder() {
 		return folder;
